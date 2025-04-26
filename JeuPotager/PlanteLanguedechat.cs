3a1050f866c8131108501024732ab89a6912641f
d-hout <dhout@ensc.fr>
2025-04-26 22:11:03 +0200

    creation webcam.cs

--- JeuPotager/PlanteLanguedechat.cs
+++
@@ -1,7 +0,0 @@
-public class LangueDeChat : Plante
-{
-    public LangueDeChat()
-        : base("Langue de Chat", "fleur", "acidulé", 5, 1.5, "rapide", "élevée", "fort", "riche", 20, "mildiou", 10, 10)
-    {
-    }
-}
