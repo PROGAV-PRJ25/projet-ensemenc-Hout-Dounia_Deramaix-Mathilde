f92ad127c9c2c0bb3930ddb6493d8e7a71b59d88
d-hout <dhout@ensc.fr>
2025-04-26 22:12:17 +0200

    creation webcam.cs (cette classe avait été creée au mauvais emplacement )

---
+++ JeuPotager/Webcam.cs
@@ -0,0 +1,41 @@
+public class Webcam
+{
+    private Random random;
+    public bool IntrusDetecte { get; private set; }
+    public bool IntemperieDetectee { get; private set; }
+
+    public Webcam()
+    {
+        random = new Random();
+        IntrusDetecte = false;
+        IntemperieDetectee = false;
+    }
+
+    // Vérification meteo
+    public void SurveillerMeteo(Meteo meteo)
+    {
+        IntemperieDetectee = meteo.EstIntemperie;
+
+        if (IntemperieDetectee)
+        {
+            Console.WriteLine("intempéries détectées dans votre jardin");
+        }
+    }
+
+    // Génère aléatoirement la présence d'un intrus
+    public void SurveillerIntrus()
+    {
+        int chance = 10; // 10% de chance
+        IntrusDetecte = random.Next(1, 101) <= chance;
+
+        if (IntrusDetecte)
+        {
+            Console.WriteLine("intrus repéré dans le potager !");
+        }
+    }
+
+
+}
+//ensuite: a ajouter dans le programme principal 
+// webcam.SurveillerMeteo(meteo);
+//webcam.SurveillerIntrus();
