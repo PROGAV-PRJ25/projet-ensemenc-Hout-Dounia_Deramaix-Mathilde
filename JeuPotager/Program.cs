26bd4314d0d2168ea138d59713857ac744fb12f7
MathildeDrmx <mderamaix@ensc.fr>
2025-04-27 15:22:44 +0200

    Améliorations de l'affichage

--- JeuPotager/Program.cs
+++ JeuPotager/Program.cs
@@ -7,59 +7,90 @@ string[] bienvenueENSC = {
             " | |_) | |  __/ | | \\ V /  __/ | | | |_| |  __/ \\__ \\ |_| | |    | |____| |\\  |____) |  __/ | | | | |  __/ | | | |____  |_|",
             " |____/|_|\\___|_| |_|\\_/ \\___|_| |_|\\__,_|\\___| |___/\\__,_|_|    |______|_| \\_|_____/ \\___|_| |_| |_|\\___|_| |_|\\_____| (_)"
         };
-
+string appelationPays = "";
+int tailleCartePays = 11;
+int tailleCarteTerrain = 13;
 string[] hariboWorld = {
-            "                                                           _________________________________",
-            "                                                          |                                 |",
-            "                                                          |     ___      .----.      ___    |",
-            "                                                          |     \\  '-.  /      \\  .-'  /    |",
-            "                                                          |      > -=.\\/        \\/.=- <     |",
-            "                                                          |      > -='/\\        /\'=- <      |",
-            "                                                          |     /__.-'  \\      /  '-.__\\    |",
-            "                                                          |              '-..-'             |",
-            "                                                          |                                 |",
-            "                                                          |        a. Haribo World          |",
-            "                                                          |_________________________________|",
+            " _________________________________",
+            "|                                 |",
+            "|     ___      .----.      ___    |",
+            "|     \\  '-.  /      \\  .-'  /    |",
+            "|      > -=.\\/        \\/.=- <     |",
+            "|      > -='/\\        /\'=- <      |",
+            "|     /__.-'  \\      /  '-.__\\    |",
+            "|              '-..-'             |",
+            "|                                 |",
+            "|        1. Haribo World          |",
+            "|_________________________________|",
         };
 
-string[] LangueDeChat = {
-            "                                                           _________________________________",
-            "                                                          |                                 |",
-            "                                                          |     ___      .----.      ___    |",
-            "                                                          |     \\  '-.  /      \\  .-'  /    |",
-            "                                                          |      > -=.\\/        \\/.=- <     |",
-            "                                                          |      > -='/\\        /\'=- <      |",
-            "                                                          |     /__.-'  \\      /  '-.__\\    |",
-            "                                                          |              '-..-'             |",
-            "                                                          |                                 |",
-            "                                                          |        a. Terrain Acidulé       |",
-            "                                                          |                                 |",
-            "                                                          |      semis : Langue de chat     |",
-            "                                                          |_________________________________|",
+string[] autrePays = {
+    " _________________________________",
+    "|                                 |",
+    "|              #####              |",
+    "|             #     #             |",
+    "|                  #              |",
+    "|                #                |",
+    "|                #                |",
+    "|                                 |",
+    "|                *                |",
+    "|         2. Autres pays          |",
+    "|_________________________________|",
+};
+
+
+string[] langueDeChat = {
+            " _________________________________",
+            "|                                 |",
+            "|     ___      .----.      ___    |",
+            "|     \\  '-.  /      \\  .-'  /    |",
+            "|      > -=.\\/        \\/.=- <     |",
+            "|      > -='/\\        /\'=- <      |",
+            "|     /__.-'  \\      /  '-.__\\    |",
+            "|              '-..-'             |",
+            "|                                 |",
+            "|        1. Terrain Acidulé       |",
+            "|                                 |",
+            "|      semis : Langue de chat     |",
+            "|_________________________________|",
         };
 
-string[] Dragibus = {
-            "                                                           _________________________________",
-            "                                                          |                                 |",
-            "                                                          |     ___      .----.      ___    |",
-            "                                                          |     \\  '-.  /      \\  .-'  /    |",
-            "                                                          |      > -=.\\/        \\/.=- <     |",
-            "                                                          |      > -='/\\        /\'=- <      |",
-            "                                                          |     /__.-'  \\      /  '-.__\\    |",
-            "                                                          |              '-..-'             |",
-            "                                                          |                                 |",
-            "                                                          |        b. Terrain Sucré         |",
-            "                                                          |                                 |",
-            "                                                          |        semis : Dragibus         |",
-            "                                                          |_________________________________|",
+string[] dragibus = {
+            " _________________________________",
+            "|                                 |",
+            "|     ___      .----.      ___    |",
+            "|     \\  '-.  /      \\  .-'  /    |",
+            "|      > -=.\\/        \\/.=- <     |",
+            "|      > -='/\\        /\'=- <      |",
+            "|     /__.-'  \\      /  '-.__\\    |",
+            "|              '-..-'             |",
+            "|                                 |",
+            "|        2. Terrain Sucré         |",
+            "|                                 |",
+            "|        semis : Dragibus         |",
+            "|_________________________________|",
         };
+
+string[] terrainVide = {
+    " _________________________________",
+    "|                                 |",
+    "|              #####              |",
+    "|             #     #             |",
+    "|                  #              |",
+    "|                #                |",
+    "|                #                |",
+    "|                                 |",
+    "|                *                |",
+    "|         X. Terrain vide         |",
+    "|                                 |",
+    "|        semis : Rien             |",
+    "|_________________________________|",
+};
 
 bool plantesMortes = false;
 bool partiefinie = false;
 
 int compteurMois = 0;
-string appelationPays;
-string appelationTerrain;
 
 // Affichage de bienvenue
 Console.WriteLine();
@@ -77,52 +108,64 @@ Console.WriteLine();
 Console.WriteLine("Veuillez sélectionner un pays pour votre potager à de son numéro parmi les pays suivants :");
 Console.WriteLine();
 
-for (int i = 0; i < hariboWorld.Length; i++)
+// Affichage et choix des pays disponibles
+for (int i = 0; i < tailleCartePays; i++)
 {
     Console.Write(hariboWorld[i]);
-    Console.WriteLine();
+    Console.Write("   ");
+    Console.WriteLine(autrePays[i]);
 }
 
+Console.WriteLine();
 ConsoleKeyInfo touchePays = Console.ReadKey();
-while (touchePays.KeyChar != 'a')
+while (touchePays.KeyChar != '1')
 {
     Console.WriteLine();
     Console.WriteLine("Erreur. Réessayez.");
     touchePays = Console.ReadKey();
 }
 
-if (touchePays.KeyChar == 'a')
+if (touchePays.KeyChar == '1')
 {
+    appelationPays = "Haribo World";
     Console.WriteLine();
-    Console.WriteLine("Veuillez sélectionner un terrain du pays choisi pour votre potager parmi les terrains disponibles :");
+    Console.WriteLine($"Veuillez sélectionner un terrain de {appelationPays} pour votre potager :");
+}
+else if (touchePays.KeyChar == '2')
+{
+    appelationPays = "Autre pays";
+    Console.WriteLine();
+    Console.WriteLine($"Veuillez sélectionner un terrain de {appelationPays} pour votre potager :");
 }
 
-for (int i = 0; i < LangueDeChat.Length; i++)
+
+// Affichage et choix des pays disponibles
+
+if (appelationPays == "Haribo World")
 {
-    Console.Write(LangueDeChat[i]);
-    Console.WriteLine();
+    for (int i = 0; i < tailleCarteTerrain; i++)
+    {
+        Console.Write(langueDeChat[i]);
+        Console.Write("   ");
+        Console.WriteLine(dragibus[i]);
+    }
 }
 
-for (int i = 0; i < Dragibus.Length; i++)
-{
-    Console.Write(Dragibus[i]);
-    Console.WriteLine();
-}
-
+Console.WriteLine();
 ConsoleKeyInfo toucheTerrain = Console.ReadKey();
-while ((toucheTerrain.KeyChar != 'a') && (toucheTerrain.KeyChar != 'b'))
+while ((toucheTerrain.KeyChar != '1') && (toucheTerrain.KeyChar != '2'))
 {
     Console.WriteLine();
     Console.WriteLine("Erreur. Réessayez.");
     toucheTerrain = Console.ReadKey();
 }
 
-if (toucheTerrain.KeyChar == 'a')
+if (toucheTerrain.KeyChar == '1')
 {
     Console.WriteLine();
     Console.WriteLine("Vous avez choisi le terrain acidulé (semis : Langue de chat acidulée)");
 }
-if (toucheTerrain.KeyChar == 'b')
+if (toucheTerrain.KeyChar == '2')
 {
     Console.WriteLine();
     Console.WriteLine("Vous avez choisi le terrain sucré (semis : Dragibus)");
