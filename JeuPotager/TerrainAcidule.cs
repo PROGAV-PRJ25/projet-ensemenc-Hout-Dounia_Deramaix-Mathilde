e0e2dd5d9cfb8b996c5cabf9f6d338164e9e6a11
d-hout <dhout@ensc.fr>
2025-05-12 11:59:58 +0200

    terrains classe heritieres

---
+++ JeuPotager/TerrainAcidule.cs
@@ -0,0 +1,7 @@
+public class TerrainAcidule : Terrain
+{
+    public TerrainAcidule(Meteo meteo)
+        : base("Langue de Chat", 20, 4, 5, "acidulé", "modéré", 18, 35.0, meteo)
+    {
+    }
+}
---
+++ JeuPotager/TerrainSucre.cs
@@ -0,0 +1,7 @@
+public class TerrainSucre : Terrain
+{
+    public TerrainSucre(Meteo meteo)
+        : base("Dragibus", 20, 4, 5, "sucré", "modéré", 20, 40.0, meteo)
+    {
+    }
+}
