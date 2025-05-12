94ec9ef3cee8ad8a07728c0821674268cbe66946
MathildeDrmx <mderamaix@ensc.fr>
2025-05-12 09:51:48 +0200

    Création Classes héritières de Plante (Dragibus et LangueDeChat)

---
+++ JeuPotager/Dragibus.cs
@@ -0,0 +1,18 @@
+public class Dragibus : Plante
+{
+    public Dragibus() : base(
+        nom: "Dragibus",
+        nature: "Bonbon dur",
+        solPref: "Croquant",
+        espacement: 0.7,
+        surfaceNecessaire: 0.3,
+        vitesseCroissance: "Modérée",
+        besoinEau: "Faible",
+        besoinLum: "Moyen",
+        besoinNutriment: "Coloré",
+        temperaturePref: 18,
+        esperanceVie: 10,
+        productionmax: 10)
+    {
+    }
+}
--- JeuPotager/PlanteDragibus.cs
+++
@@ -1,8 +0,0 @@
-/* public class Dragibus : Plante
-{
-    public Dragibus()
-        : base("Dragibus", "fruit", "sucré", 4, 1.2, "moyenne", "moyenne", "modéré", "moyen", 18, "rouille", 8, 8, new List<string> { "printemps", "été" })
-    {
-    }
-}
- */
